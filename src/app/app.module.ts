31a11409fde649b2540a8ce9d69c3ad9fb72aae5
unknown <Mason Yageman>
2020-06-03 14:30:02 -0400

    new master branch with view tasks added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TaskListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../interfaces/task'
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+  tasks: Task[];
+  taskTitle:string;
+  idForTask: number;
+
+
+  constructor(){
+
+  }
+
+
+  ngOnInit(): void {
+
+    this.idForTask = 3;
+    this.taskTitle ='';
+    this.tasks = [
+      {
+        'id':1,
+        'title':"This is task number 1",
+        'completed':false,
+        'editing':false,
+      },
+      {
+        'id':2,
+        'title':"This is task number 2",
+        'completed':false,
+        'editing':false,
+      },
+    ];
+  }
+
+
+}
