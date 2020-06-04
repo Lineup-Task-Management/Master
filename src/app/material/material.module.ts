7a342b5b387eac0fa295e6664d35a444f98fd6b5
unknown <Mason Yageman>
2020-06-04 15:47:01 -0400

    more modules

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from "@angular/material/button"
+import {MatButtonModule} from "@angular/material/button";
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+
+
 
 const MaterialComponents = [
   MatCardModule,
-  MatButtonModule
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatDividerModule,
+  MatListModule,
 
 ]
 
