23b3252479c4b5368f553ab0029a9c234fe75521
Harchint <65728326+Harchint@users.noreply.github.com>
2020-06-08 01:14:29 -0400

    Adds Complete Task Function
    Adds Function that allows task to be marked as completed.

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -36,5 +36,52 @@ export class TaskListComponent implements OnInit {
     ];
   }
 
+<<<<<<< Updated upstream
+=======
+  deleteTask(id: number){
+    this.tasks = this.tasks.filter(tasks => tasks.id != id);
+  }
+
+  addTaskItem(): void  {
+    if (this.taskTitle.trim().length === 0){
+      return;
+    }
+    this.tasks.push({
+      id: this.idForTask,
+      title: this.taskTitle,
+      completed: false,
+      editing: false,
+      description: ""
+    })
+
+    this.taskTitle = '';
+    this.idForTask++;
+  }
+
+  edit(id:number) {
+
+    let title =this.tasks[id-1].title;
+    let result = prompt("Edit Task Title", title);
+    let result1 = prompt("edit task description", this.tasks[id-1].description);
+    if (result1 !== null && result1 !== "") {
+      this.tasks[id - 1].description = result1;
+    }
+    if (result !== null && result !== "") {
+      this.tasks[id-1].title = result;
+    }
+
+  }
+
+  complete(id: number,completed:boolean){
+    let taskCompletion = this.tasks[id-1].completed;
+
+    this.tasks[id-1].completed = true;
+  }
+  
+
+
+
+
+>>>>>>> Stashed changes
 
 }
