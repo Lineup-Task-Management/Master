9e1ebd560df1d9bb3e7f0bd79da73e8e1043d20c
shanejx <dz0502@wayne.edu>
2020-06-16 16:11:10 -0400

    Due Date Component

---
+++ src/app/dist/app.module.js
@@ -0,0 +1,40 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.AppModule = void 0;
+var platform_browser_1 = require("@angular/platform-browser");
+var core_1 = require("@angular/core");
+var forms_1 = require("@angular/forms");
+var app_component_1 = require("./app.component");
+var task_list_component_1 = require("./components/task-list/task-list.component");
+var animations_1 = require("@angular/platform-browser/animations");
+var material_module_1 = require("./material/material.module");
+var light_dark_component_1 = require("./components/light-dark/light-dark.component");
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            declarations: [
+                app_component_1.AppComponent,
+                task_list_component_1.TaskListComponent,
+                light_dark_component_1.LightDarkComponent
+            ],
+            imports: [
+                platform_browser_1.BrowserModule,
+                animations_1.BrowserAnimationsModule,
+                material_module_1.MaterialModule,
+                forms_1.FormsModule,
+            ],
+            providers: [],
+            bootstrap: [app_component_1.AppComponent]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
